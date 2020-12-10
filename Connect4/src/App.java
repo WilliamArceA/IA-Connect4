e743de03e42640820188ea3de35a21de0f688c99
William Arce Aguirre <72477703+WilliamArceAguirre@users.noreply.github.com>
2020-12-09 22:44:13 -0400

    Add files via upload

---
+++ Connect4/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+   
+    public static void main (String []Args)
+    {
+        Juego a=new Juego();
+        a.pack();
+        a.setSize(950,790);
+        a.setResizable(false);
+        a.show();
+    }
+}
---
+++ Connect4/src/Mensaje.java
@@ -0,0 +1,36 @@
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.Event;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Label;
+
+public class Mensaje extends Dialog{
+    Frame f;
+    Button aceptar = new Button ("OK");
+    Label texto = new Label ();
+    String cad;
+    Mensaje(Frame f, String s){
+                super (f,"informacion");
+                setSize (400,100);
+                setResizable(false);
+                setBackground(Color.white);
+                texto.setText(s);
+                add("North",texto);
+                add("South",aceptar);
+    }
+    
+    public void paint (Graphics g){
+                g.setColor (Color.white);
+                g.drawString (cad,50,50);
+    }
+    
+    public boolean action (Event evt, Object obj){
+        if(evt.target == aceptar){
+            dispose();
+            return true;
+        }
+        return false;
+    }
+}
